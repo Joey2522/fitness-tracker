1aa47b6b4df9121fd5a1dfd775dd374eb97d6f87
Kacie O'Neill <hello@kacieo.io>
2024-02-13 16:01:01 -0800

    +npm, bcrypt, fixed routing event listeners

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+//even listeners for login button
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('loginBtn').addEventListener('click', function(){
+        window.location.href = "../views/login.handlebars";
+        console.log("Login button clicked");
+    });
+});
+
+//even listeners for create account button
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('createAcctBtn').addEventListener('click', function(){
+        window.location.href = "../views/create-acct.handlebars";
+        console.log("Create account button clicked");
+    });
+});
