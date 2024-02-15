0c04348305a76c604caa6503ef1e6ed16ad06a6b
Kacie O'Neill <hello@kacieo.io>
2024-02-14 16:27:36 -0800

    created table for login creds

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,14 @@
 DROP DATABASE IF EXISTS runTracker_db;
 
 CREATE DATABASE runTracker_db;
+
+USE runTracker_db;
+
+CREATE TABLE (
+    id INT(6) NOT NULL AUTO-INCREMENT
+    , firstname VARCHAR(30) NOT NULL 
+    , lastname VARCHAR(30) NOT NULL 
+    , email VARCHAR(99) NOT NULL
+    , password VARCHAR(30) NOT NULL
+    , PRIMARY KEY (id)
+)
