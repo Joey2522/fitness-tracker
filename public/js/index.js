ba8d6010f73d5169eb9accead9209bce62232841
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2024-02-15 19:53:56 -0600

    working submit button

--- public/js/index.js
+++ public/js/index.js
@@ -7,13 +7,14 @@ let title = document.getElementById('title');
 signinBtn.onclick = function() {
     firstName.style.maxHeight = "0";
     lastName.style.maxHeight = "0";
+    title.innerHTML = "Sign In";
     signupBtn.classList.add("disable");
     signinBtn.classList.remove("disable");
 };
 
 signupBtn.onclick = function() {
-    firstName.style.maxHeight = "65px";
-    lastName.style.maxHeight = "65px";
+    firstName.style.maxHeight = "30px";
+    lastName.style.maxHeight = "30px";
     title.innerHTML = "Sign Up";
     signupBtn.classList.remove("disable");
     signinBtn.classList.add("disable");
