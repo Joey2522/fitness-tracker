24905d607e1737cf7463408aaaef38a4015d9053
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2024-02-15 18:52:38 -0600

    added last name field

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,19 @@
 let signupBtn = document.getElementById('signupBtn');
 let signinBtn = document.getElementById('signinBtn');
-let nameField = document.getElementById('nameField');
+let firstName = document.getElementById('firstName');
+let lastName = document.getElementById('lastName');
 let title = document.getElementById('title');
 
 signinBtn.onclick = function() {
-    nameField.style.maxHeight = "0";
-    title.innerHTML = "Sign In";
+    firstName.style.maxHeight = "0";
+    lastName.style.maxHeight = "0";
     signupBtn.classList.add("disable");
     signinBtn.classList.remove("disable");
 };
 
 signupBtn.onclick = function() {
-    nameField.style.maxHeight = "65px";
+    firstName.style.maxHeight = "65px";
+    lastName.style.maxHeight = "65px";
     title.innerHTML = "Sign Up";
     signupBtn.classList.remove("disable");
     signinBtn.classList.add("disable");
